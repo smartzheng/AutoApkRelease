e79d1bd1576df670aa7540156d047efb1f146560
smartzheng <1213952997@qq.com>
2018-04-02 10:40:41 +0800

    Update android.py

--- android.py
+++ android.py
@@ -19,10 +19,36 @@ def copyFiles(sourceDir, targetDir):
             if not os.path.exists(targetDir):
                 os.makedirs(targetDir)
             open(targetFile, "wb").write(open(sourceFile, "rb").read())
+#! python3
+# 替换代码和文件，所替换的文本内容必须是当前文件的唯一字符串（可以加长需要替换扥字符串实现唯一性）
+#如需增加要替换的代码内容或者资源，只需要增加数组并且调用替换方法即可
+import os
+#app启动名
+appNames = ["app1","app2","app3"]
+#gradle中的applicationId
+replaceText = ["text1","text2","text3"]
+#gradle中的applicationId
+appIds = ["applicationId1","applicationId2","applicationId3"]
+#输出apk的文件名
+apkNames = ["apk1","apk2","apk3"]
+#项目的资源文件夹路径
+targetDir = "/Users/smartzheng/AndroidStudioProjects/MyApplication/app/src/main/res"
+#配置自己存放不同apk资源图片的路径,分别命名为apk1，apk2，apk3...（apkNames的每个元素）需要替换的图片必须名字对应一致
+sourceDir = "/Users/smartzheng/custom_app_imgs/"
+
+# 拷贝替换文件，传入图片路径和项目资源目录
+def copyFiles(sourceDir, targetDir):
+    for file in os.listdir(sourceDir):
+        sourceFile = os.path.join(sourceDir, file)
+        targetFile = os.path.join(targetDir, file)
+        if os.path.isfile(sourceFile):
+            if not os.path.exists(targetDir):
+                os.makedirs(targetDir)
+            open(targetFile, "wb").write(open(sourceFile, "rb").read())
         if os.path.isdir(sourceFile):
             copyFiles(sourceFile, targetFile)
 
-
+#替换文本内容，传入文件路径，原字符串，目标字符串
 def replaceText(f_path, text1, text2):
     with open(f_path, 'r', encoding='utf-8') as r:
         s = r.read()
@@ -37,30 +63,23 @@ print("begin")
 # 循环执行打包操作
 for i in range(len(appNames)):
     if i != 0:
-        # 替换C配置companyId
-        replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\src\main\java\com\rs\dhb\config\C.java',
-                    "return COMPANYID_" + appIds[i - 1], "return COMPANYID_" + appIds[i])
+        # 替换Config配置文件下的代码内容
+        replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/src/main/java/com/smartzheng/Config.java',replaceText[i - 1],replaceText[i])
+        #替换app名字
+        replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/src/main/res/values/strings.xml', appNames[i - 1],appNames[i])
+        #替換applicationId,即替换包名
+        replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/build.gradle',appIds[i-1],appIds[i])
+        #替换输出的路径
+        replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/build.gradle',apkNames[i-1],apkNames[i])
         # 替换资源图片
-        replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\src\main\res\values\strings.xml', appNames[i - 1],
-                    appNames[i])
-        replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\build.gradle',apkNames[i-1],apkNames[i])
-        #替換输出路径和文件名
-        copyFiles(sourceDir + appIds[i], targetDir)
-    # 打包
-    os.system("gradlew assembleRelease")
+        copyFiles(sourceDir + apkNames[i], targetDir)
+        # 打包(windows为gradlew assembleRelease)
+        os.system("gradle assembleRelease")
 
-    #重命名并放到单独的文件夹下
-    path = r'C:\Users\zhengshuai\Desktop'
-    # os.rename(os.path.join(path, 'dHB-release.apk'), os.path.join(path, apkNames[i]+'.apk'))
-    # copyFiles(path + apkNames[i]+'.apk', 'C:\\Users\\zhengshuai\\Desktop\\'+apkNames[i]+'\\'+apkNames[i]+'.apk')
-    # Tinker打包patch文件,还需要修改build。gradle文件中依赖apk名
-    # os.system("gradlew tinkerPatchRelease")
-
-# 回归原位
-replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\src\main\java\com\rs\dhb\config\C.java',
-            "return COMPANYID_" + appIds[len(appNames) - 1], "return COMPANYID_" + appIds[0])
-replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\src\main\res\values\strings.xml', appNames[len(appNames) - 1],
-            appNames[0])
-replaceText(r'H:\WORK_DHB_STUDIO\Android\Code\DHB\dHB\build.gradle',apkNames[len(appNames) - 1],apkNames[0])
-copyFiles(sourceDir + appIds[0], targetDir)
+# 将代码及资源文件回归到原位
+replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/src/main/java/com/smartzheng/Config.java',replaceText[len(appNames) - 1],replaceText[0])
+replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/src/main/res/values/strings.xml', appNames[len(appNames) - 1],appNames[0])
+replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/build.gradle',apkNames[len(appNames) - 1],apkNames[0])
+replaceText(r'/Users/smartzheng/AndroidStudioProjects/MyApplication/app/build.gradle',appIds[len(appIds) - 1],appIds[0])
+copyFiles(sourceDir + replaceText[0], targetDir)
 print("success")
